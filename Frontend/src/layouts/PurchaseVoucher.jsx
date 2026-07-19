7d18e4ce02fddbdf0dc995bf626dcf65a4b13ee2
riissshu <rishuraj24042000@gmail.com>
2026-07-19 14:48:45 +0530

    6th commit

--- Frontend/src/layouts/PurchaseVoucher.jsx
+++ Frontend/src/layouts/PurchaseVoucher.jsx
@@ -3,10 +3,9 @@ import PurchaseInputForm from "../forms/PurchaseInputForm.jsx";
 export default function PurchaseVoucher() {
     return (
         <div className="voucher">
-            <p>PurchaseVoucher</p>
             <div className="row row-4">
                 <div className="col">
-                   <PurchaseInputForm/>
+                    <PurchaseInputForm/>
                 </div>
             </div>
         </div>
--- Frontend/src/layouts/SalesVoucher.jsx
+++ Frontend/src/layouts/SalesVoucher.jsx
@@ -1,10 +1,8 @@
-import VoucherInput from "../forms/VoucherInput.jsx"
 import SalesInputForm from "../forms/SalesInputForm.jsx"
 
 export default function SalesVoucher() {
     return (
         <div className="voucher">
-            <p>SalesVoucher</p>
             <div className="row row-4">
                 <div className="col">
                    <SalesInputForm/>
